463ad3220ed996f2d3ffdd4707a678dfb5bd8b43
Consuelo Jimenez <jimenez.m.consuelo@gmail.com>
2015-06-29 11:55:09 -0700

    Create figure.h

---
+++ figure.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   figure.h
+ * Author: connie
+ *
+ * Created on May 14, 2015, 11:12 AM
+ */
+
+#ifndef FIGURE_H
+#define	FIGURE_H
+
+class Figure{
+
+public:
+    Figure(); 
+    virtual void draw();
+    virtual void erase();
+    void center();
+    
+private: 
+    
+};
+
+#endif	/* FIGURE_H */
+
